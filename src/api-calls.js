9232c9a198de125d179c97db9ca8f2246b22da33
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2022-03-03 16:23:02 -0700

    initial commit

---
+++ src/api-calls.js
@@ -0,0 +1,30 @@
+const fetchAllTravelerData = () => {
+  return fetch('http://localhost:3001/api/v1/travelers')
+    .then((response) => response.json())
+    .catch((error) => console.log(error))
+}
+
+const fetchAllTripData = () => {
+  return fetch('http://localhost:3001/api/v1/trips')
+    .then((response) => response.json())
+    .catch((error) => console.log(error))
+}
+
+const fetchAllDestinationData = () => {
+  return fetch('http://localhost:3001/api/v1/destinations	')
+    .then((response) => response.json())
+    .catch((error) => console.log(error))
+}
+
+const fetchOneTravelersData = () => {
+  return fetch('http://localhost:3001/api/v1/travelers/<id>')
+    .then((response) => response.json())
+    .catch((error) => console.log(error))
+}
+
+export {
+  fetchAllTravelerData,
+  fetchAllTripData,
+  fetchAllDestinationData,
+  fetchOneTravelersData,
+}
